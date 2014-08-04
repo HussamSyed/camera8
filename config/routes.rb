77547b5db480322b8e1f105a17b7650e6da2070b
Hussam Syed <hussamusyed@gmail.com>
2014-08-03 22:55:47 -0700

    added bootstrap, completed basic devise authentication, sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: "home#index"
+  devise_scope :user do
+    root :to => 'devise/sessions#new'
+  end
+  
+  get 'pages/home' => 'high_voltage/pages#show', id: 'home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
